71098378dbafd5f15797e2e6b74745c811bca856
Gioela <gioele.raiano@gmail.com>
2021-04-12 20:08:06 +0200

    it updates and cleans the AIMove script

--- Assets/Scripts/Enemies/AIMove.cs
+++ Assets/Scripts/Enemies/AIMove.cs
@@ -29,7 +29,9 @@ public class AIMove : MonoBehaviour
     [Tooltip ("If TRUE, Animator has Hitted parameter to handle the Die Animation")]
     [SerializeField] bool HasHitted;
 
-    int direction;
+    //[SerializeField] AnimationClip Idle;
+    //[SerializeField] AnimationClip Patrolling;
+
     bool collidableX, collidableY;
     Vector2 startPosition;
     Animator anim;
@@ -40,26 +42,21 @@ public class AIMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //direction = 1;
-
         collidableX = false;
-        rb2d = GetComponent<Rigidbody2D>();
 
         startPosition = transform.position;
         minY = startPosition.y - FloatingY ;
         maxY = startPosition.y + FloatingY ;
         
         anim = GetComponent<Animator>();
-
-
+        rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
 
         if (IsFlying)
         {
             rb2d.gravityScale = 0;
         }
-        Debug.Log($"rb2d.gravityScale: {rb2d.gravityScale}");
-
+        
         if (DirectionX == 1)
         {
             sr.flipX = !sr.flipX;
@@ -68,12 +65,13 @@ public class AIMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (collidableX )
+        if (collidableX)
         {
             DirectionX = -DirectionX;
             sr.flipX = !sr.flipX;
             collidableX = false;
         }
+
         #region comment code
         //transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime, transform.position.y, 0);
         //Vector2 dir = (Speed * 1) + (Physics2D.gravity * rb2d.gravityScale);
@@ -81,15 +79,34 @@ public class AIMove : MonoBehaviour
         #endregion
         if (IsFlying)
         {
-            //Debug.Log("IS FLYING");
-            //if (collidableY)
+            #region COMMENTED handle vertical collision
+            //if (collidableY && collidableX)
+            //{
+            //    DirectionY = -DirectionY;
+            //    DirectionX = -DirectionX;
+
+            //    collidableY = false;
+            //    collidableX = false;
+
+            //    sr.flipX = !sr.flipX;
+            //}
+            //else if (collidableY)
             //{
             //    DirectionY = -DirectionY;
             //    collidableY = false;
+            //    DirectionX = -DirectionX;
+            //    collidableX = false;
+            //}
+            //else if (collidableX)
+            //{
+            //    DirectionX = -DirectionX;
+            //    sr.flipX = !sr.flipX;
+            //    collidableX = false;
             //}
             //else
             //{
-                if (rb2d.position.y >= maxY)
+            #endregion
+            if (rb2d.position.y >= maxY)
                 {
                     Debug.Log("rb2d.position.y >= maxY");
                     DirectionY *= -1;
@@ -104,14 +121,7 @@ public class AIMove : MonoBehaviour
                     Debug.Log("T'ho fregato!");
                 }
             //}
-
             rb2d.velocity = new Vector2(DirectionX * SpeedX, DirectionY * SpeedY);
-
-            //if (collidableY)
-            //{
-            //    DirectionY = -DirectionY;
-            //    collidableY = false;
-            //}
         }
         else
         {
@@ -124,29 +134,41 @@ public class AIMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //anim.GetParameter(2pippo) = fale
-
         for (int layerIndex = 0; layerIndex < CollidableLayersName.Length; layerIndex++)
         {
             if (collision.gameObject.layer.Equals(LayerMask.NameToLayer(CollidableLayersName[layerIndex])))
             {
                 Debug.Log($"I ({this.name}) collided with {collision.gameObject.name} in layer {LayerMask.LayerToName(collision.gameObject.layer) }");
                 collidableX = true;
-                //collidableY = true;
 
-                //if (collision.transform.position.x >= transform.position.x || collision.transform.position.x <= transform.position.x)
+                #region COMMENTED handle collision Y
+                //if (IsFlying)
                 //{
-                //    collidableX = true;
-                //}
-                //else if (collision.transform.position.y >= transform.position.y || collision.transform.position.y >= transform.position.y)
-                //{
-                //    collidableY = true;
+                //    //Ray2D rayHorz = new Ray2D(rb2d.position, new Vector2(DirectionX, 0) );
+                //    //Ray2D rayVert = new Ray2D(rb2d.position, new Vector2(0, DirectionY) );
+
+                //    bool hor = Physics2D.Raycast(rb2d.position, new Vector2(DirectionX, 0), rb2d.transform.localScale.x / 2 + 0.2f) ;
+                //    bool vert = Physics2D.Raycast(rb2d.position, new Vector2(0, DirectionY), rb2d.transform.localScale.x / 2 + 0.2f) ;
+
+                //    if ( vert )
+                //    {
+                //        collidableY = true;
+                //    }
+                //    else if ( hor)
+                //    {
+                //        collidableX = true;
+                //    }
+                //    else
+                //    {
+                //        collidableY = true;
+                //        collidableX = true;
+                //    }
                 //}
                 //else
                 //{
                 //    collidableX = true;
-                //    collidableY = true;
                 //}
+                #endregion
 
                 break;
             }
