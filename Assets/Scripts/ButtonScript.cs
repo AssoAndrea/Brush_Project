87c5f147f7998243ec2eada90b96486af88ef4b6
Jekyllina <dianasternini@hotmail.it>
2021-04-21 18:42:11 +0200

    added button

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public bool Activated;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Activated)
+        {
+            anim.SetTrigger("Active");
+            Activated = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.gameObject.layer == 8)
+        {
+            Activated = true;
+        }
+    }
+}
